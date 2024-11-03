3588002dcf16613eb7d90d80648618b54008f369
Karor9 <58850040+Karor9@users.noreply.github.com>
2024-11-03 11:32:04 +0100

    [53] Map Generation

--- Scripts/Map/Map.cs
+++ Scripts/Map/Map.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public partial class Map : TileMapLayer
@@ -8,19 +9,14 @@ public partial class Map : TileMapLayer
     int height = 300;
     byte[,] MapArray;
     float _WaterLevel = 0f;
-    [ExportCategory("Camera")]
-    [Export] Camera2D camera;
-    [Export] float _CameraMovement = 10f;
-    [Export] float _Speed = 5f;
 
     float _MoveCamera = 0f;
     Vector2 mousePosition;
-    Vector2I screen;
     public override void _Ready()
     {
         altitude.Frequency = 0.003f;
+        MapArray = new byte[width, height];
         SetMap();
-        SetupCamera();
     }
 
     void SetMap()
@@ -33,6 +29,7 @@ public partial class Map : TileMapLayer
                 SetCell(x, y);
             }
         }
+        altitude.Free();
     }
 
     void SetCell(int x, int y)
@@ -41,50 +38,30 @@ public partial class Map : TileMapLayer
         {
             case float z when z >= 0.625f:
                 SetCell(new Vector2I(x, y), 0, new Vector2I(2, 0));
+                MapArray[x, y] = 2;
                 return;
             case float z when z <= _WaterLevel:
                 SetCell(new Vector2I(x, y), 0, new Vector2I(3, 3));
+                MapArray[x, y] = 15;
                 return;
             case float z when z > _WaterLevel && z < 0.0425:
                 SetCell(new Vector2I(x, y), 0, new Vector2I(0, 2));
                 return;
             default:
-                if(GD.Randf() < 0.25)
-                    SetCell(new Vector2I(x, y), 0, new Vector2I(2, 1));
-                else
-                    SetCell(new Vector2I(x, y), 0, new Vector2I(1, 2));
+                SetGround(x, y);
                 return;
         }
     }
 
-    public override void _Process(double delta)
+    private void SetGround(int x, int y)
     {
-        // if(Generate)
-        // {
-        //     Generate = !Generate;
-        //     SetMap();
-        // }
-        mousePosition = GetViewport().GetMousePosition();
-        if(mousePosition.X <= screen.X / 10 && camera.Position.X > camera.LimitLeft && camera.Position.X > screen.X/2)
-            camera.Position += new Vector2(-1 * _MoveCamera, 0);
-        if(mousePosition.X >= screen.X - (screen.X / 10) && camera.Position.X < camera.LimitRight  && camera.Position.X < (width * 16) - screen.X/2)
-            camera.Position += new Vector2(_MoveCamera, 0);
-        if(mousePosition.Y <= screen.Y / 10 && camera.Position.Y > camera.LimitTop && camera.Position.Y > screen.Y/2)
-            camera.Position += new Vector2(0, _MoveCamera * -1);
-        if(mousePosition.Y >= screen.Y - (screen.Y / 10) && camera.Position.Y < camera.LimitBottom && camera.Position.Y < (height * 16) - screen.Y/2)
-            camera.Position += new Vector2(0, _MoveCamera);
+        if(GD.Randf() < 0.25)
+        {
+            SetCell(new Vector2I(x, y), 0, new Vector2I(2, 1));
+            MapArray[x, y] = 6;
+            return;
+        }
+        SetCell(new Vector2I(x, y), 0, new Vector2I(1, 2));
+        MapArray[x, y] = 9;
     }
-
-
-    void SetupCamera()
-    {
-        screen = DisplayServer.ScreenGetSize();
-        _MoveCamera = _Speed * _CameraMovement;
-        camera.LimitRight = 16 * width;
-        camera.LimitBottom = 16 * height;
-        camera.Position = new Vector2I(width * 8, height * 8);
-        // camera.Offset = new Vector2I(-1 * screen.X / 2, -1*screen.Y / 2);
-    }
-
-
 }
